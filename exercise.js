55a08298a7bf7cc734e2c2eb0dcddbc131940e18
Damiano <d.dilionardo@gmail.com>
2023-06-26 17:48:49 +0200

    esercizi svolti in classe

--- exercise.js
+++ exercise.js
@@ -12,28 +12,46 @@
 
  
 
+
+
+function booleanToString(b) {
+    //let newElement;
+    if (b) {
+        return "true";
+    } else {
+        return "false";
+    }
+    //return newElement;
+}
+
+console.log(booleanToString(true) === "true");
+console.log(booleanToString(false) === "false");
+
+
+
+
+
 // operazioni matematiche
 
 
 function basicOp(operation, value1, value2) {
-   
-if (operation === '+') {
-    return 'false';
-} else if (operation === '-'){
-    return 'false';
-}else if (operation === '*') {
-    return 'false';
-} else if (operation === '/') {
-    return 'false';
- }
+
+    if (operation === "+") {
+        return value1 + value2;
+    }
+    if (operation === "-") {
+        return value1 - value2;
+    }
+    if (operation === "*") {
+        return value1 * value2;
+    } 
+        
+    return value1 / value2;
+    
+
 }
 
-
-
-console.log(basicOp('+', 4, 7) === 11);
-
-console.log(basicOp('-', 15, 18) === -3);
-
-console.log(basicOp('*', 5, 5) === 25);
-
-console.log(basicOp('/', 49, 7) === 7);
+console.log(basicOp("+", 4, 7) === 11);
+console.log(basicOp("-", 15, 18) === -3);
+console.log(basicOp("*", 5, 5) === 25);
+console.log(basicOp("/", 49, 7) === 7);
